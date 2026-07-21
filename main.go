d51387d079499f79dde686929849ea6c41b2e302
Vaibhav <vaibhav24597@iiitd.ac.in>
2026-07-21 20:51:15 +0530

    add basic http server with health endpoint

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "ok")
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("Server starting on port 8080...")
+	http.ListenAndServe(":8080", nil)
 }
