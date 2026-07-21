49253edcb408033ad02d64abc5d7847fb2aad48a
Vaibhav <vaibhav24597@iiitd.ac.in>
2026-07-21 21:00:29 +0530

    connect go server to postgres

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/jackc/pgx/v5"
 )
+
+var dbConn *pgx.Conn
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "ok")
 }
 
+func connectDB() {
+	connString := "postgres://minidrive:minidrive123@localhost:5433/minidrive_db"
+
+	conn, err := pgx.Connect(context.Background(), connString)
+	if err != nil {
+		fmt.Println("Unable to connect to database:", err)
+		return
+	}
+	dbConn = conn
+	fmt.Println("Connected to Postgres successfully!")
+}
+
 func main() {
+	connectDB()
 	http.HandleFunc("/health", healthHandler)
 
 	fmt.Println("Server starting on port 8080...")
